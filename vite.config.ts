9f4689272300b6f1b51a754ef60a16dcc4d682e1
David D'Amico <damico.david97@gmail.com>
2025-06-28 19:18:47 +0200

    v.0.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,10 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: '/einfach-sprechen-community/', // replace with your actual GitHub repo name if different
   plugins: [
     react(),
     mode === 'development' &&
